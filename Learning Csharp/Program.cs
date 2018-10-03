d3fab9cb345f54859d9f0550471cd4c783d90ebf
Vamsi <vamsi.are@jato.com>
2018-10-03 09:23:56 +0100

    Commit chnage

--- Learning Csharp/Program.cs
+++ Learning Csharp/Program.cs
@@ -10,6 +10,29 @@ namespace Learning_Csharp
     {
         static void Main(string[] args)
         {
+            /*
+         INT X;
+         INT Y;
+
+         X=3 ;
+         Y=X+5;
+         CONSOLE.WRITELINE(Y); */
+         
+
+         // Print Name ,Defining Strings and Concatenarion
+         Console.WriteLine("What is your Name?");
+         Console.Write("Type your First Name: ");
+         string FirstName;
+         FirstName = Console.ReadLine();
+         
+         string LastName ;
+         Console.Write("Type your Last Name: ");
+         LastName = Console.ReadLine();
+
+         Console.WriteLine("Hello, " + FirstName + " " + LastName);
+         //Console.ReadLine();
+         
         }
+
     }
 }
